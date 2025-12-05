a756480b0488d32da8108305853e69fa2926f4eb
egerstle <eitan.gerstle@gmail.com>
2025-12-05 10:47:44 -0800

    Update 2_stream_mapping.R

--- code/2_stream_mapping.R
+++ code/2_stream_mapping.R
@@ -19,26 +19,6 @@ opt_chunk_buffer(snoho_lascat)<-1
 
 snoho_rast<-rasterize_terrain(snoho_lascat, 
                               res = 0.5,
-                              algorithm = kriging())
+                              algorithm = tin())
 
-wbt_breach_depressions_least_cost(
-  dem = snoho_rast,
-  output = "snoho_breach.tif"
-)
-
-wbt_fill_depressions_wang_and_liu(
-  dem = "snoho_breach.tif",
-  output = here("Data","Raters",paste("snoho_breach_fill.tif"))
-)
-
-wbt_d8_flow_accumulation(
-  input = here("Data","Raters",paste("snoho_breach_fill.tif")),
-  output = "snoho_FA.tif"
-)
-
-wbt_extract_streams(
-  flow_accum = "snoho_FA.tif",
-  output = here("Data","Rasters","snoho_streams.tif"),
-  threshold = 2000
-)
 
